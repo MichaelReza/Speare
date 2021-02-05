14d6167f94943ddfc8778f2916bd173de6fc35af
Cameron Gould <cam@gouldcs.com>
2021-02-04 17:22:26 -0800

    updated test kit to fully work

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,27 +1,45 @@
 import assert from "assert"
-import parse from "../src/parse.js"
+import parse from "../src/parser.js"
 
-const goodPrograms = [
-  "alloweth myVar be \"hello\"",
-  "speaketh(x accumulate 5)",
-  "x with 5 without 6",
-  "x accumulate 6 sunder 12",
-  "enter ToBeOrNotToBe foo(Numeral x) { \
-  alloweth x be 1 \
-  return x \
-  }",
-  "x decrement",
-  "x decrementby 4",
-  "x increment",
-  "x incrementby 4",
-  "x be x decrement"
-]
+const goodPrograms = {
+  "Recognizes a simple print statement" : "alloweth myVar be \"hello\"",
+  "Recognizes a print statement" : "speaketh(x accumulate 5)",
+  "Recognizes arithmetic +-" : "x with 5 without 6",
+  "Recognizes arithmetic */" : "x accumulate 6 sunder 12",
+  "Recognizes functions" : "enter ToBeOrNotToBe foo(Numeral x) { \
+        alloweth x be 1 \
+        return x \
+        }",
+  "Recognizes decrementation" : "x decrement",
+  "Recognizes specified decrementation" : "x decrementby 4",
+  "Recognizes incrememntation" : "x increment",
+  "Recognizes specified incrememntation" : "x incrementby 4",
+  "Recognizes assignment to decrementation" : "x be x decrement"
+}
 
-const badPrograms = [
-  "",
-  "alloweth",
-  "alloweth increment be",
-  "allow x",
-  "allow x be increment",
-  "enter increment foo(Numeral increment) {}"
-]
+const badPrograms = {
+  "Disallows empty program" : "",
+  "Disallows incomplete statement" : "alloweth",
+  "Disallows incomplete statement" : "alloweth increment be",
+  "Disallows declaration" : "allow x",
+  "Disallows assignment to keyword" : "allow x be increment",
+  "Disallows non-type function" : "enter increment foo(Numeral increment) {}"
+}
+
+describe("the Parser", () => {
+  
+  // Accepted
+  for (const [prompt, code] of Object.entries(goodPrograms)) {
+    console.log(prompt + "aaa")
+      it(prompt, () => {
+        assert.ok(parse(code))
+      })
+  }
+
+  // Recejcted
+  for (const [prompt, code] of Object.entries(badPrograms)) {
+      it(prompt, () => {
+        assert.ok(parse(code))
+      })
+  }
+})
