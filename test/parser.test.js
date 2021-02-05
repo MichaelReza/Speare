9bfa52ea8d32c88b23f1b1f0954087d49f0c80e5
Cameron Gould <cam@gouldcs.com>
2021-02-04 19:24:55 -0800

    formatted tester

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,28 +2,70 @@ import assert from "assert"
 import parse from "../src/parser.js"
 
 const goodPrograms = {
+
+  // Empty Program:
   "Recognizes empty program" : "",
+
+  // Printing:
   "Recognizes a simple print statement" : "alloweth myVar be \"hello\"",
+
+  // Variable Declaration:
+
+  // Arithmetic:
   "Recognizes a print statement" : "speaketh(x accumulate 5)",
-  "Recognizes arithmetic +-" : "x = x with 5 without 6)",
-  "Recognizes arithmetic */" : "x accumulate 6 sunder 12",
-  "Recognizes functions" : "enter ToBeOrNotToBe foo(Numeral x) { \
-        alloweth x be 1 \
-        return x \
-        }",
+  "Recognizes arithmetic +-" : "x be x with 5 without 6",
+  "Recognizes arithmetic */" : "y be x accumulate 6 sunder 12",
   "Recognizes decrementation" : "x decrement",
   "Recognizes specified decrementation" : "x decrementby 4",
   "Recognizes incrememntation" : "x increment",
   "Recognizes specified incrememntation" : "x incrementby 4",
-  "Recognizes assignment to decrementation" : "x be x decrement"
+  "Recognizes assignment to decrementation" : "x be x decrement",
+
+  // Functions:
+  "Recognizes functions" : "enter ToBeOrNotToBe foo(Numeral x) { \
+        alloweth x be 1 \
+        returneth x \
+        }",
+  // Classes:
+
+  // For loops:
+
+  // While Loops:
+
+  // Do-While Loops:
+
+  // IF-statements:
+
+  // Switch-Case Statements:
+
 }
 
 const badPrograms = {
-  "Disallows incomplete statement" : "alloweth",
+  "Disallows declaration" : "alloweth x",
+  "Disallows assignment to keyword" : "alloweth x be increment",
+  "Disallows non-type function" : "enter increment foo(Numeral increment) {}",
+  // Printing:
+
+  // Variable Declaration:
+ "Disallows incomplete statement" : "alloweth",
+  // Arithmetic:
+
+  // Functions:
+
+  // Classes:
+
+  // For loops:
+
+  // While Loops:
+
+  // Do-While Loops:
+
+  // IF-statements:
+
+  // Switch-Case Statements:
+
+  // Types:
   "Disallows incomplete statement" : "alloweth increment be",
-  "Disallows declaration" : "allow x",
-  "Disallows assignment to keyword" : "allow x be increment",
-  "Disallows non-type function" : "enter increment foo(Numeral increment) {}"
 }
 
 describe("the Parser", () => {
@@ -39,7 +81,7 @@ describe("the Parser", () => {
   // Recejcted
   for (const [prompt, code] of Object.entries(badPrograms)) {
       it(prompt, () => {
-        assert.ok(parse(code))
+        assert.rejects(parse(code))
       })
   }
 })
