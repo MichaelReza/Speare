0bb4df1f9abd06a61f9da7f6e945384acb2eb01e
caitlincolina <caitlincolina@gmail.com>
2021-03-23 00:41:33 -0700

    Tests done but can someone run?

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -81,130 +81,125 @@ const semanticChecks = [
   "alloweth Numeral x be 1 \
   speaketh(2 accumulate 3 with 5 exponentiate nay(3) sunder 2 without 5 residue 8)"
   ],
-  ["variables", "let x=[[[[1]]]]; print(x[0][0][0][0]+2);"],
-  ["recursive structs", "struct S {z: S?} let x = S(no S);"],
-  ["nested structs", "struct T{y:int} struct S{z: T} let x=S(T(1)); print(x.z.y);"],
-  ["member exp", "struct S {x: int} let y = S(1);print(y.x);"],
-  ["subscript exp", "let a=[1,2];print(a[0]);"],
-  ["array of struct", "struct S{} let x=[S(), S()];"],
-  ["struct of arrays and opts", "struct S{x: [int] y: string??}"],
-  ["assigned functions", "function f() {}\nlet g = f;g = f;"],
-  ["call of assigned functions", "function f(x: int) {}\nlet g=f;g(1);"],
+  ["variables", "alloweth Liste x be [[[[1]]]] speaketh(x[0][0][0][0] with 2)"],
+  // ["recursive structs", "struct S {z: S?} let x = S(no S)"],
+  // ["nested structs", "struct T{y:int} struct S{z: T} let x=S(T(1)) speaketh(x.z.y)"],
+  // ["member exp", "struct S {x: int} let y = S(1)speaketh(y.x)"],
+  // ["subscript exp", "let a=[1,2]speaketh(a[0])"],
+  // ["array of struct", "struct S{} let x=[S(), S()]"],
+  // ["struct of arrays and opts", "struct S{x: [int] y: string??}"],
+  // ["assigned functions", "function f() {}\nlet g = fg = f"],
+  ["call of assigned functions", "enter Numeral f(Numeral x) {}\nalloweth Numeral g be f g(1)"],
   [
     "call of assigned function in expression",
-    `function f(x: int, y: boolean): int {}
-    let g = f;
-    print(g(1, true));
-    f = g; // Type check here`,
+    `enter Indistinguishable f(Numeral x, ToBeOrNotToBe y) {}
+    alloweth Enter g be f
+    speaketh(g(1, true))
+    f be g // Type check here`,
   ],
   [
     "pass a function to a function",
-    `function f(x: int, y: (boolean)->void): int { return 1; }
-     function g(z: boolean) {}
-     f(2, g);`,
+    `enter Numeral f(Numeral x, ToBeOrNotToBe y) { returneth 1 }
+     enter Indistinguishable g(ToBeOrNotToBe z) {}
+     f(2, g)`,
   ],
   [
     "function return types",
-    `function square(x: int): int { return x * x; }
-     function compose(): (int)->int { return square; }`,
+    `enter Numeral square(Numeral x) { returneth x accumulate x }
+     enter Numeral compose() { returneth square }`,
   ],
-  ["struct parameters", "struct S {} function f(x: S) {}"],
-  ["array parameters", "function f(x: [int?]) {}"],
-  ["optional parameters", "function f(x: [int], y: string?) {}"],
-  ["built-in constants", "print(25.0 * π);"],
-  ["built-in sin", "print(sin(π));"],
-  ["built-in cos", "print(cos(93.999));"],
-  ["built-in hypot", "print(hypot(-4.0, 3.00001));"],
+  //["struct parameters", "struct S {} function f(x: S) {}"],
+  // ["array parameters", "enter Indistinguishable f(Numeral? x) {}"],
+  // ["optional parameters", "enter Indistinguishable f(Numeral x, Lexicographical? y) {}"],
 ]
 
 // Programs that are syntactically correct but have semantic errors
 const semanticErrors = [
   ["non-distinct fields", "struct S {x: boolean x: int}", /Fields must be distinct/],
-  ["non-int increment", "let x=false;x++;", /an integer, found boolean/],
-  ["non-int decrement", 'let x=some[""];x++;', /an integer, found [string]?/],
-  ["undeclared id", "print(x);", /Identifier x not declared/],
-  ["redeclared id", "let x = 1;let x = 1;", /Identifier x already declared/],
-  ["assign to const", "const x = 1;x = 2;", /Cannot assign to constant x/],
-  ["assign bad type", "let x=1;x=true;", /Cannot assign a boolean to a int/],
-  ["assign bad array type", "let x=1;x=[true];", /Cannot assign a \[boolean\] to a int/],
-  ["assign bad optional type", "let x=1;x=some 2;", /Cannot assign a int\? to a int/],
-  ["break outside loop", "break;", /Break can only appear in a loop/],
+  ["non-int increment", "alloweth Numeral x be fallacious x increment", /an integer, found boolean/],
+  ["non-int decrement", 'alloweth Numeral x = Lexicographical("")x++', /an integer, found [string]?/], //TO-DO bc some[""] is optional and I am confused
+  ["undeclared id", "speaketh(x)", /Identifier x not declared/],
+  ["redeclared id", "alloweth Numeral x be 1 alloweth Numeral x be 1", /Identifier x already declared/],
+  // ["assign to const", "const x = 1x = 2", /Cannot assign to constant x/],
+  ["assign bad type", "alloweth Numeral x be 1 alloweth Numeral x  be faithful", /Cannot assign a boolean to a int/],
+  ["assign bad array type", "alloweth Numeral x be 1 alloweth Liste x be [true]", /Cannot assign a \[boolean\] to a int/],
+  // ["assign bad optional type", "let x=1x=some 2", /Cannot assign a int\? to a int/],
+  ["break outside loop", "exit", /Break can only appear in a loop/],
   [
     "break inside function",
-    "while true {function f() {break;}}",
+    "whilst(x){execute{exit}}",
     /Break can only appear in a loop/,
   ],
-  ["return outside function", "return;", /Return can only appear in a function/],
+  ["return outside function", "returneth", /Return can only appear in a function/],
   [
     "return value from void function",
-    "function f() {return 1;}",
+    "enter f() {returneth 1}",
     /Cannot return a value here/,
   ],
   [
     "return nothing from non-void",
-    "function f(): int {return;}",
+    "enter Numeral f(){ returneth }",
     /should be returned here/,
   ],
-  ["return type mismatch", "function f(): int {return false;}", /boolean to a int/],
-  ["non-boolean short if test", "if 1 {}", /a boolean, found int/],
-  ["non-boolean if test", "if 1 {} else {}", /a boolean, found int/],
-  ["non-boolean while test", "while 1 {}", /a boolean, found int/],
-  ["non-integer repeat", 'repeat "1" {}', /an integer, found string/],
-  ["non-integer low range", "for i in true...2 {}", /an integer, found boolean/],
-  ["non-integer high range", "for i in 1..<no int {}", /an integer, found int\?/],
-  ["non-array in for", "for i in 100 {}", /Array expected/],
-  ["non-boolean conditional test", "print(1?2:3);", /a boolean, found int/],
-  ["diff types in conditional arms", "print(true?1:true);", /not have the same type/],
-  ["unwrap non-optional", "print(1??2);", /Optional expected/],
-  ["bad types for ||", "print(false||1);", /a boolean, found int/],
-  ["bad types for &&", "print(false&&1);", /a boolean, found int/],
-  ["bad types for ==", "print(false==1);", /Operands do not have the same type/],
-  ["bad types for !=", "print(false==1);", /Operands do not have the same type/],
-  ["bad types for +", "print(false+1);", /number or string, found boolean/],
-  ["bad types for -", "print(false-1);", /a number, found boolean/],
-  ["bad types for *", "print(false*1);", /a number, found boolean/],
-  ["bad types for /", "print(false/1);", /a number, found boolean/],
-  ["bad types for **", "print(false**1);", /a number, found boolean/],
-  ["bad types for <", "print(false<1);", /number or string, found boolean/],
-  ["bad types for <=", "print(false<=1);", /number or string, found bool/],
-  ["bad types for >", "print(false>1);", /number or string, found bool/],
-  ["bad types for >=", "print(false>=1);", /number or string, found bool/],
-  ["bad types for ==", "print(2==2.0);", /not have the same type/],
-  ["bad types for !=", "print(false!=1);", /not have the same type/],
-  ["bad types for negation", "print(-true);", /a number, found boolean/],
-  ["bad types for length", "print(#false);", /Array expected/],
-  ["bad types for not", 'print(!"hello");', /a boolean, found string/],
-  ["non-integer index", "let a=[1];print(a[false]);", /integer, found boolean/],
-  ["no such field", "struct S{} let x=S(); print(x.y);", /No such field/],
-  ["diff type array elements", "print([3,3.0]);", /Not all elements have the same type/],
-  ["shadowing", "let x = 1;\nwhile true {let x = 1;}", /Identifier x already declared/],
-  ["call of uncallable", "let x = 1;\nprint(x());", /Call of non-function/],
+  ["return type mismatch", "enter Numeral f(){returneth fallacious}", /boolean to a int/],
+  ["non-boolean short if test", "whether (1) {}", /a boolean, found int/],
+  ["non-boolean if test", "whether (1) {} otherwise {}", /a boolean, found int/],
+  ["non-boolean while test", "whilest (1) {}", /a boolean, found int/],
+  // ["non-integer repeat", 'repeat "1" {}', /an integer, found string/],
+  // ["non-integer low range", "for i in true...2 {}", /an integer, found boolean/],
+  // ["non-integer high range", "for i in 1..<no int {}", /an integer, found int\?/],
+  ["non-array in for", "in regards to(i within 100) {}", /Array expected/],
+  // ["non-boolean conditional test", "speaketh(1?2:3) ", /a boolean, found int/],
+  // ["diff types in conditional arms", "speaketh(true?1:true) ", /not have the same type/],
+  // ["unwrap non-optional", "speaketh(1??2) ", /Optional expected/],
+  ["bad types for ||", "speaketh(fallacious alternatively 1) ", /a boolean, found int/],
+  ["bad types for &&", "speaketh(fallacious furthermore 1) ", /a boolean, found int/],
+  ["bad types for ==", "speaketh(fallacious tis 1) ", /Operands do not have the same type/],
+  ["bad types for !=", "speaketh(fallacious tis 1) ", /Operands do not have the same type/],
+  ["bad types for +", "speaketh(fallacious with 1) ", /number or string, found boolean/],
+  ["bad types for -", "speaketh(fallacious without 1) ", /a number, found boolean/],
+  ["bad types for *", "speaketh(fallacious accumulate 1) ", /a number, found boolean/],
+  ["bad types for /", "speaketh(fallacious sunder 1) ", /a number, found boolean/],
+  ["bad types for **", "speaketh(fallacious exponentiate 1) ", /a number, found boolean/],
+  ["bad types for <", "speaketh(fallacious lesser 1) ", /number or string, found boolean/],
+  ["bad types for <=", "speaketh(fallacious tis lesser 1) ", /number or string, found bool/],
+  ["bad types for >", "speaketh(fallacious nobler 1) ", /number or string, found bool/],
+  ["bad types for >=", "speaketh(fallacious tis nobler 1) ", /number or string, found bool/],
+  ["bad types for ==", "speaketh(2 tis 2.0) ", /not have the same type/],
+  ["bad types for !=", "speaketh(fallacious nay be 1) ", /not have the same type/],
+  ["bad types for negation", "speaketh(-faithful) ", /a number, found boolean/],
+  ["bad types for length", "speaketh(#fallacious)", /Array expected/],
+  ["bad types for not", 'speaketh(nay"hello")', /a boolean, found string/],
+  ["non-integer index", "alloweth a be [1]speaketh(a[fallacious])", /integer, found boolean/],
+  // ["no such field", "struct S{} let x=S() speaketh(x.y)", /No such field/],
+  ["diff type array elements", "speaketh([3,3.0])", /Not all elements have the same type/],
+  ["shadowing", "alloweth x be 1\nwhilest (faithful) {alloweth x be 1}", /Identifier x already declared/],
+  ["call of uncallable", "alloweth x be 1\nspeaketh(x())", /Call of non-function/],
   [
     "Too many args",
-    "function f(x: int) {}\nf(1,2);",
+    "enter Indistinguishable f(Numeral x) {}\nf(1,2)",
     /1 argument\(s\) required but 2 passed/,
   ],
   [
     "Too few args",
-    "function f(x: int) {}\nf();",
+    "enter Indistinguishable f(Numeral x) {}\nf()",
     /1 argument\(s\) required but 0 passed/,
   ],
   [
     "Parameter type mismatch",
-    "function f(x: int) {}\nf(false);",
+    "enter f(Numeral x) {}\nf(fallacious)",
     /Cannot assign a boolean to a int/,
   ],
-  [
-    "function type mismatch",
-    `function f(x: int, y: (boolean)->void): int { return 1; }
-     function g(z: boolean): int { return 5; }
-     f(2, g);`,
-    /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
-  ],
-  ["bad call to stdlib sin()", "print(sin(true));", /Cannot assign a boolean to a float/],
-  ["Non-type in param", "let x=1;function f(y:x){}", /Type expected/],
-  ["Non-type in return type", "let x=1;function f():x{return 1;}", /Type expected/],
-  ["Non-type in field type", "let x=1;struct S {y:x}", /Type expected/],
+  // [
+  //   "function type mismatch",
+  //   `enter f( Numeral x, y: (boolean)->void): int { return 1 }
+  //    enter g(z: boolean): int { return 5 }
+  //    f(2, g)`,
+  //   /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
+  // ],
+  ["Non-type in param", "alloweth Numeral x be 1 enter Indistinguishable f(x y){}", /Type expected/],
+  ["Non-type in return type", "alloweth Numeral x be 1 enter x f(){returneth 1}", /Type expected/],
+  // ["Non-type in field type", "alloweth Numeral x be 1struct S {y:x}", /Type expected/],
 ]
 
 // Test cases for expected semantic graphs after processing the AST. In general
@@ -216,9 +211,9 @@ const Int = ast.Type.INT
 const Void = ast.Type.VOID
 const intToVoidType = new ast.FunctionType([Int], Void)
 
-const varX = Object.assign(new ast.Variable("x", false), { type: Int })
+const varX = Object.assign(new ast.Variable("x", fallacious), { type: Int })
 
-const letX1 = Object.assign(new ast.VariableDeclaration("x", false, 1n), {
+const letX1 = Object.assign(new ast.VariableDeclaration("x", fallacious, 1n), {
   variable: varX,
 })
 const assignX2 = new ast.Assignment(varX, 2n)
@@ -232,10 +227,10 @@ const funDeclF = Object.assign(
   }
 )
 
-const structS = new ast.StructDeclaration("S", [new ast.Field("x", Int)])
+// const structS = new ast.StructDeclaration("S", [new ast.Field("x", Int)])
 
 const graphChecks = [
-  ["Variable created & resolved", "let x=1; x=2;", [letX1, assignX2]],
+  ["Variable created & resolved", "let x=1 x=2", [letX1, assignX2]],
   ["functions created & resolved", "function f(x: int) {}", [funDeclF]],
   ["field type resolved", "struct S {x: int}", [structS]],
 ]
