dab5e9319cd5d6dfa0d9e29ac11a3554b925583b
Cameron Gould <cam@gouldcs.com>
2021-04-13 21:25:04 -0700

    97 total tests passing!

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -116,12 +116,12 @@ const semanticChecks = [
   alloweth Concordance of Lexicographical and Concordance of Lexicographical and Concordance of Lexicographical and Numeral a be {\"x\": b}\
   speaketh(a.\"x\".\"y\".\"z\")"],
   // [26] TODO: REWRITE THIS GARBAGE
-  // [
-  //   "subscript dict",
-  //   "alloweth Concordance of Lexicographical and Numeral y be {\"x\": 5} speaketh(y.\"x\")",
-  // ],
+  [
+    "subscript dict",
+    "alloweth Concordance of Lexicographical and Numeral y be {\"x\": 5} speaketh(y.\"x\")",
+  ],
   // [27] TODO: REWRITE THIS GARBAGE
-  // ["subscript liste", "alloweth Liste of Numeral a be [1,2] speaketh(a[0])"],
+  ["subscript liste", "alloweth Liste of Numeral a be [1,2] speaketh(a[0])"],
   // [28] TODO: REWRITE THIS GARBAGE
   [
     "array of corollary",
@@ -133,173 +133,159 @@ const semanticChecks = [
     'alloweth Concordance of Lexicographical and Liste of Numeral S be {"x": [1,2,3]}',
   ],
  
-  // // [32] TODO
-  // [
-  //   "call of assigned function in expression",
-  //   `enter Indistinguishable f(Numeral x, ToBeOrNotToBe y) {}
-  //   alloweth Enter g be f
-  //   speaketh(g(1, true))
-  //   f be g // Type check here`,
-  // ],
-  // // [33] TODO
-  // [
-  //   "pass a function to a function",
-  //   `enter Numeral f (Numeral x, ToBeOrNotToBe y) { returneth 1 }
-  //   enter ToBeOrNotToBe g(ToBeOrNotToBe z) { }
-  //    f(2, g)`,
-  // ],
-  // // [34] TODO
-  // [
-  // "function return types",
-  // `enter Numeral compose( ) {
-  //   enter Numeral square(Numeral x) { returneth x accumulate x }
-  //   returneth square
-  // }`,
-  // ],
-  // [35] TODO
-  // ["struct parameters", "struct S {} function f(x: S) {}"],
+  // [32] TODO
+  [
+    "call of assigned function in expression",
+    `enter Indistinguishable f(Numeral x, ToBeOrNotToBe y) {}
+    alloweth Enter g be f
+    speaketh(g(1, true))
+    f be g // Type check here`,
+  ],
+
+  // [35] DONE
+["concordance parameters", "alloweth Concordance of Numeral and Numeral S be {5 : 12}\nenter ToBeOrNotToBe fun(Concordance of Numeral and Numeral x) {}"],
   // [36] TODO - we arent doing optionals (for now)
-  // ["array parameters",
-  // "enter Indistinguishable f(Numeral? x) {}"
-  // ],
+  ["array parameters",
+  "enter Indistinguishable f(Numeral? x) {}"
+  ],
   // [37] TODO - not supporting options (for now)
-  // ["optional parameters",
-  // "enter Indistinguishable f(Numeral x, Lexicographical? y) {}"],
+  ["optional parameters",
+  "enter Indistinguishable f(Numeral x, Lexicographical? y) {}"],
 ]
 
 // Programs that are syntactically correct but have semantic errors
 const semanticErrors = [
   // [38] TODO: REWRITE THIS GARBAGE
-  // ["non-distinct fields", "struct S {x: boolean x: int}", /Fields must be distinct/],
+  ["non-distinct fields", "struct S {x: boolean x: int}", /Fields must be distinct/],
   // [39] TODO
-  // ["non-int increment", "alloweth Numeral x be fallacious x increment", /an integer, found boolean/],
-  // // [40] TODO: REWRITE THIS GARBAGE
-  // ["non-int decrement", 'alloweth Lexicographical x = "hello" x increment', /an integer, found [string]?/], //TO-DO bc some[""] is optional and I am confused
+  ["non-int increment", "alloweth Numeral x be fallacious x increment", /an integer, found boolean/],
+  // [40] TODO: REWRITE THIS GARBAGE
+  ["non-int decrement", 'alloweth Lexicographical x = "hello" x increment', /an integer, found [string]?/], //TO-DO bc some[""] is optional and I am confused
   // [41] DONE
   ["undeclared id", "speaketh(x)", /Identifier x not declared/],
   // [42] DONE
-  // ["redeclared id", "alloweth Numeral x be 1 alloweth Numeral x be 1", /Identifier x already declared/],
-  // // [43] TODO
-  // ["assign bad type", "alloweth Numeral x be faithful", /Cannot assign a boolean to a int/],
-  // // [44] TODO
-  // ["assign bad array type", "alloweth Numeral x be 1 alloweth Liste x be [true]", /Cannot assign a \[boolean\] to a int/],
-  // // [45] TODO
-  // ["break outside loop", "exit", /Break can only appear in a loop/],
+  ["redeclared id", "alloweth Numeral x be 1 alloweth Numeral x be 1", /Identifier x already declared/],
+  // [43] TODO
+  ["assign bad type", "alloweth Numeral x be faithful", /Cannot assign a boolean to a int/],
+  // [44] TODO
+  ["assign bad array type", "alloweth Numeral x be 1 alloweth Liste x be [true]", /Cannot assign a \[boolean\] to a int/],
+  // [45] DONE
+  ["break outside loop", "exit", /Break can only appear in a loop/],
   // [46] TODO: REWRITE THIS GARBAGE
   [
     "break inside function",
     "whilst(x){execute{exit}}",
     /Break can only appear in a loop/,
   ],
-  // // [47] TODO
-  // ["return outside function", "returneth", /Return can only appear in a function/],
-  // // [48] TODO
-  // [
-  //   "return value from void function",
-  //   "enter f() {returneth 1}",
-  //   /Cannot return a value here/,
-  // ],
-  // // [49] TODO
-  // [
-  //   "return nothing from non-void",
-  //   "enter Numeral f(){ returneth }",
-  //   /should be returned here/,
-  // ],
-  // // [50] TODO
-  // ["return type mismatch", "enter Numeral f(){returneth fallacious}", /boolean to a int/],
-  // // [51] TODO
-  // ["non-boolean short if test", "whether (1) {}", /a boolean, found int/],
-  // // [52] TODO
-  // ["non-boolean if test", "whether (1) {} otherwise {}", /a boolean, found int/],
-  // // [53] TODO
-  // ["non-boolean while test", "whilest (1) {}", /a boolean, found int/],
-  // // [54] TODO
-  // ["non-integer range", "in regards to (i within faithful){}", /an integer, found boolean/],
-  // // [55] TODO
-  // ["non-array in for", "in regards to(i within 100) {}", /Array expected/],
-  // // [56] TODO
-  // ["non-boolean conditional test", "whether (1) {}", /a boolean, found int/],
-  // // [57] TODO
-  // ["diff types in conditional arms", "whether (x be 5) { x be fallacious } ", /not have the same type/],
-  // // [58] TODO
-  // ["bad types for ||", "speaketh(fallacious alternatively 1) ", /a boolean, found int/],
+  // [47] TODO
+  ["return outside function", "returneth", /Return can only appear in a function/],
+  // [48] TODO
+  [
+    "return value from void function",
+    "enter f() {returneth 1}",
+    /Cannot return a value here/,
+  ],
+  // [49] TODO
+  [
+    "return nothing from non-void",
+    "enter Numeral f(){ returneth }",
+    /should be returned here/,
+  ],
+  // [50] TODO
+  ["return type mismatch", "enter Numeral f(){returneth fallacious}", /ToBeOrNotToBe to a number/],
+  // [51] TODO
+  ["non-boolean short if test", "whether (1) {}", /a ToBeOrNotToBe, found number/],
+  // [52] TODO
+  ["non-boolean if test", "whether (1) {} otherwise {}", /a boolToBeOrNotToBeean, found number/],
+  // [53] TODO
+  ["non-boolean while test", "whilest (1) {}", /a ToBeOrNotToBe, found number/],
+  // [54] TODO
+  ["non-integer range", "in regards to (i within faithful){}", /a number, found ToBeOrNotToBe/],
+  // [55] TODO
+  ["non-array in for", "in regards to(i within 100) {}", /Array expected/],
+  // [56] TODO
+  ["non-boolean conditional test", "whether (1) {}", /a ToBeOrNotToBe, found number/],
+  // [57] TODO
+  ["diff types in conditional arms", "whether (x be 5) { x be fallacious } ", /not have the same type/],
+  // [58] TODO
+  ["bad types for ||", "speaketh(fallacious alternatively 1) ", /a ToBeOrNotToBe, found number/],
   // [59] TODO
-  ["bad types for and", "speaketh(fallacious furthermore 1) ", /a boolean, found int/],
+  ["bad types for and", "speaketh(fallacious furthermore 1) ", /a ToBeOrNotToBe, found number/],
   // [60] DONE
   ["bad types for ==", "speaketh(fallacious tis 1) ", /Operands do not have the same type/],
- // [61] DONE
+  // [61] DONE
   ["bad types for !=", "speaketh(fallacious tis 1) ", /Operands do not have the same type/],
-  // // [62] TODO
-  // ["bad types for +", "speaketh(fallacious with 1) ", /number or string, found boolean/],
-  // // [63] TODO
-  // ["bad types for -", "speaketh(fallacious without 1) ", /a number, found boolean/],
-  // // [64] TODO
-  // ["bad types for *", "speaketh(fallacious accumulate 1) ", /a number, found boolean/],
-  // // [65] TODO
-  // ["bad types for /", "speaketh(fallacious sunder 1) ", /a number, found boolean/],
-  // // [66] TODO
-  // ["bad types for **", "speaketh(fallacious exponentiate 1) ", /a number, found boolean/],
-  // // [67] TODO
-  // ["bad types for <", "speaketh(fallacious lesser 1) ", /number or string, found boolean/],
-  // // [68] TODO
-  // ["bad types for <=", "speaketh(fallacious tis lesser 1) ", /number or string, found bool/],
-  // [69] TODO, NICELY
-  // ["bad types for >", "speaketh(fallacious nobler 1) ", /number or string, found bool/],
-  // [70] TODO
-  // ["bad types for >=", "speaketh(fallacious tis nobler 1) ", /number or string, found bool/],
-  // [71] TODO
-  // ["bad types for ==", "speaketh(2 tis 2.0) ", /not have the same type/],
+  // [62] TODO
+  ["bad types for +", "speaketh(fallacious with 1) ", /number or string, found ToBeOrNotToBe/],
+  // [63] TODO
+  ["bad types for -", "speaketh(fallacious without 1) ", /a number, found ToBeOrNotToBe/],
+  // [64] TODO
+  ["bad types for *", "speaketh(fallacious accumulate 1) ", /a number, found ToBeOrNotToBe/],
+  // [65] DONE
+  ["bad types for /", "speaketh(fallacious sunder 1) ", /a number, found ToBeOrNotToBe/],
+  // [66] DONE
+  ["bad types for **", "speaketh(fallacious exponentiate 1) ", /a number, found ToBeOrNotToBe/],
+  // [67] DONE
+  ["bad types for <", "speaketh(fallacious lesser 1) ", /number or string, found ToBeOrNotToBe/],
+  // [68] DONE
+  ["bad types for <=", "speaketh(fallacious tis lesser 1) ", /number or string, found ToBeOrNotToBe/],
+  // [69] DONE
+  ["bad types for >", "speaketh(fallacious nobler 1) ", /number or string, found ToBeOrNotToBe/],
+  // [70] DONE
+  ["bad types for >=", "speaketh(fallacious tis nobler 1) ", /number or string, found ToBeOrNotToBe/],
+  // [71] DONE
+  ["bad types for ==", "speaketh(2 tis fallacious) ", /not have the same type/],
   // [72] DONE
   ["bad types for !=", "speaketh(fallacious tis not 1) ", /not have the same type/],
-  // // [73] TODO
-  // ["bad types for negation", "speaketh(nay(faithful)) ", /a number, found boolean/],
-  // // [74] TODO
-  // ["bad types for length", "speaketh(#fallacious)", /Array expected/],
-  // // [75] TODO
-  // ["bad types for not", 'speaketh(nay("hello"))', /a boolean, found string/],
-  // // [76] TODO
-  // ["non-integer index", "alloweth a be [1] speaketh(a[fallacious])", /integer, found boolean/],
-  // // [77] TODO
-  // ["diff type array elements", "speaketh([3,3.0])", /Not all elements have the same type/],
-  // // [78] TODO
-  // ["shadowing", "alloweth Numeral x be 1 \
-  //               whilst (faithful) { \
-  //                 alloweth Numeral  x be 1 \
-  //               }", /Identifier x already declared/],
-  // // [79] TODO
-  // ["call of uncallable", "alloweth x be 1\nspeaketh(x())", /Call of non-function/],
-  // // [80] TODO
-  // [
-  //   "Too many args",
-  //   "enter Indistinguishable f(Numeral x) {}\nf(1,2)",
-  //   /1 argument\(s\) required but 2 passed/,
-  // ],
-  // // [81] TODO
-  // [
-  //   "Too few args",
-  //   "enter Indistinguishable f(Numeral x) {}\nf()",
-  //   /1 argument\(s\) required but 0 passed/,
-  // ],
+  // [73] TODO
+  ["bad types for negation", "speaketh(nay(faithful)) ", /a number, found ToBeOrNotToBe/],
+  // [74] TODO
+  ["bad types for length", "speaketh(#fallacious)", /Array expected/],
+  // [75] TODO
+  ["bad types for not", 'speaketh(nay("hello"))', /a ToBeOrNotToBe, found Lexicographical/],
+  // [76] TODO
+  ["non-integer index", "alloweth a be [1] speaketh(a[fallacious])", /number, found ToBeOrNotToBe/],
+  // [77] TODO
+  ["diff type array elements", "speaketh([3,3.0])", /Not all elements have the same type/],
+  // [78] TODO
+  ["shadowing", "alloweth Numeral x be 1 \
+                whilst (faithful) { \
+                  alloweth Numeral  x be 1 \
+                }", /Identifier x already declared/],
+  // [79] TODO
+  ["call of uncallable", "alloweth x be 1\nspeaketh(x())", /Call of non-function/],
+  // [80] TODO
+  [
+    "Too many args",
+    "enter Indistinguishable f(Numeral x) {}\nf(1,2)",
+    /1 argument\(s\) required but 2 passed/,
+  ],
+  // [81] TODO
+  [
+    "Too few args",
+    "enter Indistinguishable f(Numeral x) {}\nf()",
+    /1 argument\(s\) required but 0 passed/,
+  ],
   // [82] TODO
-  // [
-  //   "Parameter type mismatch",
-  //   "enter f(Numeral x) {}\nf(fallacious)",
-  //   /Cannot assign a boolean to a int/,
-  // ],
+  [
+    "Parameter type mismatch",
+    "enter f(Numeral x) {}\nf(fallacious)",
+    /Cannot assign a boolean to a int/,
+  ],
   // [83] TODO
-  // [
-  //   "function type mismatch",
-  //   `enter f( Numeral x, y: (boolean)->void): int { return 1 }
-  //    enter g(z: boolean): int { return 5 }
-  //    f(2, g)`,
-  //   /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
-  // ],
+  [
+    "function type mismatch",
+    `enter f( Numeral x, y: (boolean)->void): int { return 1 }
+     enter g(z: boolean): int { return 5 }
+     f(2, g)`,
+    /Cannot assign a \(boolean\)->int to a \(boolean\)->void/,
+  ],
   // [84] TODO
-  // ["Non-type in param", "alloweth Numeral x be 1 enter Indistinguishable f(x y){}", /Type expected/],
+  ["Non-type in param", "alloweth Numeral x be 1 enter Indistinguishable f(x y){}", /Type expected/],
   // [85] TODO
-  // ["Non-type in return type", "alloweth Numeral x be 1 enter x f(){returneth 1}", /Type expected/],
+  ["Non-type in return type", "alloweth Numeral x be 1 enter x f(){returneth 1}", /Type expected/],
   // [86] TODO
-  // ["Non-type in field type", "alloweth Numeral x be 1struct S {y:x}", /Type expected/],
+  ["Non-type in field type", "alloweth Numeral x be 1struct S {y:x}", /Type expected/],
 ]
 
 describe("The analyzer", () => {
@@ -308,11 +294,11 @@ describe("The analyzer", () => {
       assert.ok(analyze(parse(source)))
     })
   }
-  // for (const [scenario, source, errorMessagePattern] of semanticErrors) {
-  //   it(`throws on ${scenario}`, () => {
-  //     assert.throws(() => analyze(parse(source)), errorMessagePattern)
-  //   })
-  // }
+  for (const [scenario, source, errorMessagePattern] of semanticErrors) {
+    it(`throws on ${scenario}`, () => {
+      assert.throws(() => analyze(parse(source)), errorMessagePattern)
+    })
+  }
   // for (const [scenario, source, graph] of graphChecks) {
   //   it(`properly rewrites the AST for ${scenario}`, () => {
   //     assert.deepStrictEqual(analyze(parse(source)), new ast.Program(graph))
